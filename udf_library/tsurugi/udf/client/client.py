c85980a7d98d765203e740f1ee6f7b6fe572f0df
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2025-12-04 16:26:36 +0900

    add abstract

--- udf_library/tsurugi/udf/client/client.py
+++ udf_library/tsurugi/udf/client/client.py
@@ -2,34 +2,46 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from tsurugi.udf.model import tsurugi_types_pb2
 
-
 class BlobRelayClient(ABC):
-
+    """ A client for BLOB relay service. """
     def download(self, ref, destination: Path):
-        if isinstance(ref, tsurugi_types_pb2.BlobReference):
-            return self.download_blob(ref, destination)
-        if isinstance(ref, tsurugi_types_pb2.ClobReference):
-            return self.download_clob(ref, destination)
-        raise TypeError("Unknown reference type")
+        """ Download BLOB data identified by `blob_ref` and save it to `destination`.
+
+        Args:
+            blob_ref (BlobReference): The reference to the BLOB to download.
+            destination (Path): The file path where the downloaded BLOB data will be saved.
+
+        Raises:
+            BlobRelayError: If an error occurs in the BLOB relay service.
+            BlobRelayError: If there is an error during communication.
+            OSError: If there is an error writing to the destination file.
+        """
+        return self.download_blob(ref, destination)
 
     def upload(self, source: Path):
-        if self._is_binary(source):
-            return self.upload_blob(source)
-        else:
-            return self.upload_clob(source)
+        """ Upload BLOB data from `source` and return a reference to the uploaded BLOB.
+
+        Args:
+            source (Path): The file path of the BLOB data to upload.
+
+        Returns:
+            BlobReference: A reference to the uploaded BLOB.
+
+        Raises:
+            OSError: If there is an error reading from the source file.
+            BlobRelayError: If there is an error during communication.
+            BlobRelayError: If an error occurs in the BLOB relay service.
+        """
+        return self.upload_blob(source)
+
 
     @abstractmethod
-    def download_blob(self, ref: tsurugi_types_pb2.BlobReference, destination: Path):
-        pass
+    def download_blob(self, ref: tsurugi_types_pb2.BlobReference, destination: Path): pass
 
     @abstractmethod
-    def upload_blob(self, source: Path) -> tsurugi_types_pb2.BlobReference:
-        pass
+    def upload_blob(self, source: Path) -> tsurugi_types_pb2.BlobReference: pass
 
-    @abstractmethod
-    def download_clob(self, ref: tsurugi_types_pb2.ClobReference, destination: Path):
-        pass
 
-    @abstractmethod
-    def upload_clob(self, source: Path) -> tsurugi_types_pb2.ClobReference:
-        pass
+
+
+

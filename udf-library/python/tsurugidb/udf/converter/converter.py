ab61e741e462a4a15597b76b498438c10ec4cd1e
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2025-12-12 22:21:07 +0900

    docs: add docstring to converter.py

--- udf-library/python/tsurugidb/udf/converter/converter.py
+++ udf-library/python/tsurugidb/udf/converter/converter.py
@@ -1,6 +1,6 @@
 from datetime import date, time, datetime, timedelta, timezone
 from decimal import Decimal
-from tsurugidb.udf.model.tsurugi_types_pb2 import (
+from ..model.tsurugi_types_pb2 import (
     Decimal as PbDecimal,
     Date as PbDate,
     LocalTime as PbLocalTime,
@@ -8,20 +8,27 @@ from tsurugidb.udf.model.tsurugi_types_pb2 import (
     OffsetDatetime as PbOffsetDatetime,
 )
 
-
 def to_pb_decimal(value: Decimal) -> PbDecimal:
+    """Converts a standard library Decimal to Protocol Buffer Decimal message.
+
+    Args:
+        value: The decimal value to convert.
+
+    Returns:
+        Protocol Buffer message of the decimal.
+
+    Raises:
+        ValueError: If the decimal value is infinite or NaN.
+    """
+    if not value.is_finite():
+        raise ValueError("Only finite decimal values are supported.")
+
     sign, digits, exponent = value.as_tuple()
     unscaled_int = 0
-    # -123.45
-    # sign  -1
-    # digits 1,2,3,4,5
-    # exponent -2
     for d in digits:
         unscaled_int = unscaled_int * 10 + d
-    # 12345
     if sign == 1:
         unscaled_int = -unscaled_int
-    # -12345
     if unscaled_int == 0:
         unscaled_bytes = b"\x00"
     else:
@@ -38,95 +45,160 @@ def to_pb_decimal(value: Decimal) -> PbDecimal:
     return pb
 
 
-def from_pb_decimal(pb: PbDecimal) -> Decimal:
-    unscaled_int = int.from_bytes(pb.unscaled_value, byteorder="big", signed=True)
+def from_pb_decimal(message: PbDecimal) -> Decimal:
+    """Converts a Protocol Buffer Decimal message to standard library Decimal.
 
-    exponent = pb.exponent
+    Args:
+        message: Protocol Buffer message of the decimal to convert.
 
+    Returns:
+        Standard library Decimal value.
+    """
+
+    unscaled_int = int.from_bytes(message.unscaled_value, byteorder="big", signed=True)
+    exponent = message.exponent
     return Decimal(unscaled_int) * (Decimal(10) ** exponent)
 
 
-# message Date {
-#  // the number of days since 1970-01-01.
-#  sint32 days = 1;
-# }
-EPOCH_DATE = date(1970, 1, 1)
+_EPOCH_DATE = date(1970, 1, 1)
 
+def to_pb_date(value: date) -> PbDate:
+    """Converts a standard library date to Protocol Buffer Date message.
 
-def to_pb_date(d: date) -> PbDate:
-    delta = d - EPOCH_DATE
+    Args:
+        value: The date value to convert.
+
+    Returns:
+        Protocol Buffer message of the date.
+    """
+    delta = value - _EPOCH_DATE
     return PbDate(days=delta.days)
 
 
-def from_pb_date(pb_date: PbDate) -> date:
-    return EPOCH_DATE + timedelta(days=pb_date.days)
+def from_pb_date(message: PbDate) -> date:
+    """Converts a Protocol Buffer Date object to standard library date.
+
+    Args:
+        message: Protocol Buffer message of the date to convert.
+
+    Returns:
+        Standard library date value.
+    """
+    return _EPOCH_DATE + timedelta(days=message.days)
 
 
-# message LocalTime {
-#  // the number of nanoseconds since midnight.
-#  sint64 nanos = 1;
-# }
-def to_pb_local_time(t: time) -> PbLocalTime:
+def to_pb_local_time(value: time) -> PbLocalTime:
+    """Converts a standard library time to Protocol Buffer LocalTime message.
+
+    This ignores the timezone information of the input time.
+
+    Args:
+        value: The time value to convert.
+
+    Returns:
+        Protocol Buffer message of the time.
+    """
+    value = value.replace(tzinfo=None)
     nanos = (
-        t.hour * 3600_000_000_000
-        + t.minute * 60_000_000_000
-        + t.second * 1_000_000_000
-        + t.microsecond * 1000
+        value.hour * 3_600_000_000_000
+        + value.minute * 60_000_000_000
+        + value.second * 1_000_000_000
+        + value.microsecond * 1000
     )
     # time does not have nanosecond precision, so microsecond * 1000
     return PbLocalTime(nanos=nanos)
 
 
-def from_pb_local_time(pb_time: PbLocalTime) -> time:
-    total_nanos = pb_time.nanos
-    hours, rem = divmod(total_nanos, 3600_000_000_000)
+def from_pb_local_time(message: PbLocalTime) -> time:
+    """Converts a Protocol Buffer LocalTime message to standard library time.
+
+    The returned time will have no timezone information (tzinfo=None).
+    Nanosecond precision is truncated to microsecond precision.
+
+    Args:
+        message: Protocol Buffer message of the time to convert.
+
+    Returns:
+        Standard library time without timezone information.
+    """
+    total_nanos = message.nanos
+    hours, rem = divmod(total_nanos, 3_600_000_000_000)
     minutes, rem = divmod(rem, 60_000_000_000)
     seconds, nanos = divmod(rem, 1_000_000_000)
     microseconds = nanos // 1000
-    return time(hour=hours, minute=minutes, second=seconds, microsecond=microseconds)
-
-
-# message LocalDatetime {
-#  // offset seconds from epoch (1970-01-01 00:00:00) at the local time zone.
-#  sint64 offset_seconds = 1;
-#  // nano-seconds adjustment [0, 10^9-1].
-#  uint32 nano_adjustment = 2;
-# }
-EPOCH_DATETIME = datetime(1970, 1, 1)
-
-
-def to_pb_local_datetime(dt: datetime) -> PbLocalDatetime:
-    delta = dt - EPOCH_DATETIME
-    offset_seconds = int(delta.total_seconds())
-    nano_adjustment = dt.microsecond * 1000
-    return PbLocalDatetime(
-        offset_seconds=offset_seconds, nano_adjustment=nano_adjustment
+    return time(
+        hour=hours,
+        minute=minutes,
+        second=seconds,
+        microsecond=microseconds
     )
 
 
-def from_pb_local_datetime(pb_dt: PbLocalDatetime) -> datetime:
-    dt = EPOCH_DATETIME + timedelta(
-        seconds=pb_dt.offset_seconds, microseconds=pb_dt.nano_adjustment // 1000
+_EPOCH_DATETIME = datetime(1970, 1, 1)
+
+def to_pb_local_datetime(value: datetime) -> PbLocalDatetime:
+    """Converts a standard library datetime to Protocol Buffer LocalDatetime message.
+
+    This ignores the timezone information of the input datetime.
+
+    Args:
+        value: The datetime value to convert.
+
+    Returns:
+        Protocol Buffer message of the datetime.
+    """
+    value = value.replace(tzinfo=None)
+    delta = value - _EPOCH_DATETIME
+    offset_seconds = int(delta.total_seconds())
+    nano_adjustment = value.microsecond * 1000
+    return PbLocalDatetime(
+        offset_seconds=offset_seconds,
+        nano_adjustment=nano_adjustment
+    )
+
+def from_pb_local_datetime(message: PbLocalDatetime) -> datetime:
+    """Converts a Protocol Buffer LocalDatetime message to standard library datetime.
+
+    The returned datetime will have no timezone information (tzinfo=None).
+
+    Args:
+        message: Protocol Buffer message of the datetime to convert.
+
+    Returns:
+        Standard library datetime without timezone information.
+    """
+
+    dt = _EPOCH_DATETIME + timedelta(
+        seconds=message.offset_seconds,
+        microseconds=message.nano_adjustment // 1000
     )
     return dt
 
+def to_pb_offset_datetime(value: datetime) -> PbOffsetDatetime:
+    """Converts a standard library datetime to Protocol Buffer OffsetDatetime message.
 
-# message OffsetDatetime {
-#  // offset seconds from epoch (1970-01-01 00:00:00Z).
-#  sint64 offset_seconds = 1;
-#  // nano-seconds adjustment [0, 10^9-1].
-#  uint32 nano_adjustment = 2;
-#  // timezone offset in minute.
-#  sint32 time_zone_offset = 3;
-# }
-def to_pb_offset_datetime(dt: datetime) -> PbOffsetDatetime:
-    if dt.tzinfo is None:
+    If the input datetime has no timezone information (tzinfo is None), it is treated
+    as a naive datetime and the timezone offset will be set to 0 (UTC). However, when
+    converting to UTC using astimezone(), the system's local timezone may be assumed.
+
+    For datetime objects with timezone information, the offset is preserved and the
+    datetime is converted to UTC for storage. Note that Python's datetime has microsecond
+    precision, which is converted to nanosecond precision by multiplying by 1000.
+
+    Args:
+        value: The datetime value to convert.
+
+    Returns:
+        Protocol Buffer message of the datetime.
+    """
+
+    if value.tzinfo is None:
         tz_offset_min = 0
     else:
-        tz_offset_min = int(dt.utcoffset().total_seconds() // 60)
-    delta = dt.astimezone(timezone.utc) - EPOCH_DATETIME.replace(tzinfo=timezone.utc)
+        tz_offset_min = int(value.utcoffset().total_seconds() // 60)
+    delta = value.astimezone(timezone.utc) - _EPOCH_DATETIME.replace(tzinfo=timezone.utc)
     offset_seconds = int(delta.total_seconds())
-    nano_adjustment = dt.microsecond * 1000
+    nano_adjustment = value.microsecond * 1000
     return PbOffsetDatetime(
         offset_seconds=offset_seconds,
         nano_adjustment=nano_adjustment,
@@ -134,12 +206,23 @@ def to_pb_offset_datetime(dt: datetime) -> PbOffsetDatetime:
     )
 
 
-def from_pb_offset_datetime(pb_dt: PbOffsetDatetime) -> datetime:
-    dt_utc = EPOCH_DATETIME.replace(tzinfo=timezone.utc) + timedelta(
-        seconds=pb_dt.offset_seconds,
-        microseconds=pb_dt.nano_adjustment // 1000,
+def from_pb_offset_datetime(message: PbOffsetDatetime) -> datetime:
+    """Converts a Protocol Buffer OffsetDatetime message to standard library datetime.
+
+    Nanosecond precision is truncated to microsecond precision.
+
+    Args:
+        message: Protocol Buffer message of the datetime to convert.
+
+    Returns:
+        Standard library datetime with timezone information.
+    """
+
+    dt_utc = _EPOCH_DATETIME.replace(tzinfo=timezone.utc) + timedelta(
+        seconds=message.offset_seconds,
+        microseconds=message.nano_adjustment // 1000,
     )
-    tz = timezone(timedelta(minutes=pb_dt.time_zone_offset))
+    tz = timezone(timedelta(minutes=message.time_zone_offset))
     return dt_utc.astimezone(tz)
 
 __all__ = [

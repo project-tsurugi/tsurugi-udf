e66ec994d5644c7709010c8ed4c3e08ae91b78f3
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2025-12-17 17:46:06 +0900

    add test and fix

--- udf-plugin/builder/tests/test_basic2.py
+++ udf-plugin/builder/tests/test_basic2.py
@@ -3,19 +3,21 @@ from udf_plugin_builder import build_with_cmake
 from pathlib import Path
 
 
-def test_basic_build():
+def test_basic_build2():
     with tempfile.TemporaryDirectory() as tmp_dir:
         build_with_cmake.run(
             [
                 "--name",
-                "plugin_api_test",
+                "plugin_api_test2",
                 "--proto-file",
                 "tests/proto/sample.proto",
+                "--proto-path",
+                "tests",
             ]
         )
         for fname in [
-            "libplugin_api_test.so",
-            "libplugin_api_test.ini",
+            "libplugin_api_test2.so",
+            "libplugin_api_test2.ini",
         ]:
             p = Path(fname)
             if p.exists():
--- udf-plugin/builder/tests/test_types.py
+++ udf-plugin/builder/tests/test_types.py
@@ -3,19 +3,23 @@ from udf_plugin_builder import build_with_cmake
 from pathlib import Path
 
 
-def test_basic_build():
+def test_types_build():
     with tempfile.TemporaryDirectory() as tmp_dir:
         build_with_cmake.run(
             [
                 "--name",
-                "plugin_api_test",
+                "types_test",
                 "--proto-file",
-                "tests/proto/sample.proto",
+                "tests/proto/read_tsurugi_types.proto",
+                "../../proto/tsurugidb/udf/tsurugi_types.proto",
+                "--proto-path",
+                "tests",
+                "../../proto",
             ]
         )
         for fname in [
-            "libplugin_api_test.so",
-            "libplugin_api_test.ini",
+            "libtypes_test.so",
+            "libtypes_test.ini",
         ]:
             p = Path(fname)
             if p.exists():

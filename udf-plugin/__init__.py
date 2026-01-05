bf5d3a87b189580bb10ad5819fe0a3344bc31ba1
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2026-01-06 01:07:29 +0900

    remove unnessary version and fix object_schema

--- udf-plugin/__init__.py
+++ udf-plugin/__init__.py
@@ -1,6 +1,1 @@
-"""
-Top-level package for Tsurugi UDF plugin system.
-Includes builder, viewer, and common utilities.
-"""
-__version__ = "0.1.1"
 
--- udf-plugin/tsurugi_udf/builder/udf_plugin_builder/object_schema.py
+++ udf-plugin/tsurugi_udf/builder/udf_plugin_builder/object_schema.py
@@ -1,3 +1,3 @@
 from tsurugi_udf.common.tsurugi_udf_common.descriptors import Version
 # fix_version
-OBJECT_SCHEMA_VERSION = Version(0, 1, 0)
+OBJECT_SCHEMA_VERSION = Version(0, 1, 1)

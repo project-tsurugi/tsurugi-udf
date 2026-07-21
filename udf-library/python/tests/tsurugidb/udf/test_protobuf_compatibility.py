5cb427265763fe807f425a9dbd05f7ef83ab7968
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2026-07-21 18:51:44 +0900

    update copilot comment

--- udf-library/python/tests/tsurugidb/udf/test_protobuf_compatibility.py
+++ udf-library/python/tests/tsurugidb/udf/test_protobuf_compatibility.py
@@ -11,9 +11,23 @@ PB2_MODULES = [
 ]
 
 PB2_GRPC_MODULES = [
+    "tsurugidb.udf.client.grpc.blob_reference_pb2_grpc",
     "tsurugidb.udf.client.grpc.blob_relay_local_pb2_grpc",
     "tsurugidb.udf.client.grpc.blob_relay_streaming_pb2_grpc",
 ]
+
+
+def test_protobuf_runtime_supports_7_35_generated_code() -> None:
+    from google.protobuf import runtime_version
+
+    runtime_version.ValidateProtobufRuntimeVersion(
+        runtime_version.Domain.PUBLIC,
+        7,
+        35,
+        0,
+        "",
+        "compatibility_test.proto",
+    )
 
 
 @pytest.mark.parametrize("module_name", PB2_MODULES)
